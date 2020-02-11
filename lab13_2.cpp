97c304932d83862cab11282a01cd224b9b3afa20
theeramet0793 <theerametmetha@gmail.com>
2020-02-11 17:25:28 +0700

    commit-2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -43,7 +43,7 @@ void findLocalMax(const double A[][N], bool B[][N]){
 
      for(int i=1;i<N-1;i++){
 		 for(int j=1;j<N-1;j++){
-			 if(A[i-1][j]<A[i][j]&&A[i+1][j]<A[i][j]&&A[i][j-1]<A[i][j]&&A[i][j+1]<A[i][j])
+			 if(A[i-1][j]<=A[i][j]&&A[i+1][j]<=A[i][j]&&A[i][j-1]<=A[i][j]&&A[i][j+1]<=A[i][j])
 			   B[i][j]=1;
 			 else  
                 B[i][j]=0;
