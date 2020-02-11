5cb1c1c68f543e1c0e1a70888a85218e4cf4ce4a
theeramet0793 <theerametmetha@gmail.com>
2020-02-11 16:42:58 +0700

    commit-1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+int position;
 template <typename T>
 void swap(T d[],int x,int y){
     T temp = d[x];
@@ -13,6 +14,35 @@ template <typename T>
 void insertionSort(T d[],int N){
 	
 //Write your code here.
+ for(int i=1;i<N;i++){
+
+	 for(int p=0;p<N;p++){
+		if(p==i) 
+		cout<<"["<<d[p]<<"]"<<" ";
+		else
+		cout<<d[p]<<" "; 
+	 }cout<<"=>";
+
+	 for(int j=i;j>0;j--){
+		 if (d[j]>d[j-1]){
+		  swap(d,j,j-1);
+		  position=j-1;}
+		 else
+		 {
+			 if(j==i)
+			 position=i;
+		 }
+		  
+	 }
+
+	 for(int p=0;p<N;p++){
+		if(p==position) 
+		cout<<"["<<d[p]<<"]"<<" ";
+		else
+		cout<<d[p]<<" "; 
+	 }cout<<"\n";
+	 
+}
 
 }
 
